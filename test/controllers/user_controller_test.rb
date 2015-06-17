1cb2dae68d91b918effd37314e7f1223447a2883
fwpp <srnwml@gmail.com>
2015-06-17 08:41:01 +0000

    done first test ----user_controller_test instruction:      bin/rake test test/controllers/user_controller_test.rb result: controller_test.rb Run options: --seed 21170
    # Running:
    
    ...
    
    Finished in 0.202893s, 14.7861 runs/s, 14.7861 assertions/s.
    
    3 runs, 3 assertions, 0 failures, 0 errors, 0 skips

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
+   
+  def test_the_truth
+    assert true
+  end
+  
+  test "should get new page" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should get profile page" do
+    get :profile
+    assert_response :success
+  end
+  
 end
